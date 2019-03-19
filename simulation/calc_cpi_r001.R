f374bcb7c828c84e732c3eba815e4bfc74636544
VitorAguiar <vitor.aguiar@me.com>
2019-03-19 16:07:01 -0300

    update

--- simulation/calc_cpi_r001.R
+++ simulation/calc_cpi_r001.R
@@ -9,7 +9,8 @@ simul_r001 <- paste0("./results/pi_r001_chunk", 1:25, ".tsv") %>%
 
 cpi_r001_all <- simul_r001 %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(adj_pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_r001_all, "./results/cpi_r001_all.tsv")
@@ -17,7 +18,8 @@ write_tsv(cpi_r001_all, "./results/cpi_r001_all.tsv")
 cpi_r001_codis <- simul_r001 %>%
     filter(marker %in% codis_loci) %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(adj_pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_r001_codis, "./results/cpi_r001_codis.tsv")
@@ -25,7 +27,8 @@ write_tsv(cpi_r001_codis, "./results/cpi_r001_codis.tsv")
 cpi_r001_ident <- simul_r001 %>%
     filter(marker %in% ident_loci) %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(adj_pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_r001_ident, "./results/cpi_r001_ident.tsv")
@@ -33,7 +36,8 @@ write_tsv(cpi_r001_ident, "./results/cpi_r001_ident.tsv")
 cpi_r001_pp16 <- simul_r001 %>%
     filter(marker %in% pp16_loci) %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(adj_pi), 
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_r001_pp16, "./results/cpi_r001_pp16.tsv")
--- simulation/calc_cpi_strbase.R
+++ simulation/calc_cpi_strbase.R
@@ -8,7 +8,8 @@ simul_strbase <- paste0("./results/pi_strbase_chunk", 1:25, ".tsv") %>%
 
 cpi_strbase_all <- simul_strbase %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_strbase_all, "./results/cpi_strbase_all.tsv")
@@ -16,7 +17,8 @@ write_tsv(cpi_strbase_all, "./results/cpi_strbase_all.tsv")
 cpi_strbase_ident <- simul_strbase %>%
     filter(marker %in% ident_loci) %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_strbase_ident, "./results/cpi_strbase_ident.tsv")
@@ -24,7 +26,8 @@ write_tsv(cpi_strbase_ident, "./results/cpi_strbase_ident.tsv")
 cpi_strbase_pp16 <- simul_strbase %>%
     filter(marker %in% pp16_loci) %>%
     group_by(case_no) %>%
-    summarise(cpi = prod(adj_pi)) %>%
+    summarise(cpi = prod(pi),
+	      n_exclusions = sum(exclusion)) %>%
     ungroup()
 
 write_tsv(cpi_strbase_pp16, "./results/cpi_strbase_pp16.tsv")
