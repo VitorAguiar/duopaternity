0944f0cc94c71adf94a5f39a721df12f92c7a824
VitorAguiar <vitor.aguiar@me.com>
2019-03-20 13:43:38 -0300

    update

--- simulation/compile_results.R
+++ simulation/compile_results.R
@@ -1,10 +1,7 @@
 library(tidyverse)
 
-total_trios <- read_tsv("../familias_pipeline/trios/total_trios.tsv") %>%
-    select(1, 3) %>%
-    gather(i, n, -1) %>%
-    spread(marker_set, n) %>%
-    select(-i)
+
+#r001 
 
 dat_r001_all <- read_tsv("./results/cpi_r001_all.tsv")
 dat_r001_codis <- read_tsv("./results/cpi_r001_codis.tsv")
@@ -12,25 +9,24 @@ dat_r001_ident <- read_tsv("./results/cpi_r001_ident.tsv")
 dat_r001_pp16 <- read_tsv("./results/cpi_r001_pp16.tsv")
 
 inclusion_r001_codis <- dat_r001_codis %>%
-    slice(1:(total_trios$codis*1000)) %>%
-    mutate(slot = rep(1:total_trios$codis, each = 1000)) %>%
-    group_by(slot) %>%
-    filter(cpi >= 10000) %>%
-    ungroup()
+    summarise(n = mean(n_exclusions < 3 & cpi >= 10000)*100)
 
 inclusion_r001_ident <- dat_r001_ident %>%
-    slice(1:(total_trios$identifiler*1000)) %>%
-    mutate(slot = rep(1:total_trios$identifiler, each = 1000)) %>%
-    group_by(slot) %>%
-    filter(cpi >= 10000) %>%
-    ungroup()
+    summarise(n = mean(n_exclusions < 3 & cpi >= 10000)*100)
 
 inclusion_r001_pp16 <- dat_r001_pp16 %>%
-    slice(1:(total_trios$pp16*1000)) %>%
-    mutate(slot = rep(1:total_trios$pp16, each = 1000)) %>%
-    group_by(slot) %>%
-    filter(cpi >= 10000) %>%
-    ungroup()
+    summarise(n = mean(n_exclusions < 3 & cpi >= 10000)*100)
 
 
+#STRBASE
+
+dat_strbase_all <- read_tsv("./results/cpi_strbase_all.tsv")
+dat_strbase_ident <- read_tsv("./results/cpi_strbase_ident.tsv")
+dat_strbase_pp16 <- read_tsv("./results/cpi_strbase_pp16.tsv")
+
+inclusion_strbase_ident <- dat_strbase_ident %>%
+    summarise(n = mean(n_exclusions < 3 & cpi >= 10000)*100)
+
+inclusion_strbase_pp16 <- dat_strbase_pp16 %>%
+    summarise(n = mean(n_exclusions < 3 & cpi >= 10000)*100)
 
